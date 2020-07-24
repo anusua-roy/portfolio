bea33cb6d683da376024dac73a232ddcc31db7dd
Anusua Roy <68214813+anusua-roy@users.noreply.github.com>
2020-07-24 23:03:52 +0530

    Update web-portfolio.js

--- js/web-portfolio.js
+++ js/web-portfolio.js
@@ -1,1 +1,7 @@
-
+function change(obj) {
+    var a = document.getElementsByClassName("active");
+    for (i=0; i<a.length; i++) {
+        a.item(i).className = "noColor";
+    }
+    obj.className = "active";
+}
