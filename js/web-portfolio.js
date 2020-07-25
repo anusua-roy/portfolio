1597e985f908cc671fb20b13413201234d891bae
Anusua Roy <68214813+anusua-roy@users.noreply.github.com>
2020-07-25 22:01:36 +0530

    Update web-portfolio.js

--- js/web-portfolio.js
+++ js/web-portfolio.js
@@ -1,7 +1,25 @@
 function change(obj) {
-    var a = document.getElementsByClassName("active");
+    var a = document.getElementsByClassName("active link");
     for (i=0; i<a.length; i++) {
-        a.item(i).className = "noColor";
+        a.item(i).className = "noColor link";
     }
-    obj.className = "active";
+    obj.className = "active link";
+}
+
+function dchange(obj) {
+    var a = document.getElementsByClassName("dactive");
+    for (i=0; i<a.length; i++) {
+        a.item(i).className = "nodColor";
+    }
+    obj.className = "dactive";
+}
+
+function openMenu() {
+    var x = document.getElementById('dropdown');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    }
+    else {
+        x.style.display = 'none';
+    }
 }
